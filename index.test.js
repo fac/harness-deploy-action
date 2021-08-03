c5fb2f6039985e9e9b9fe1cc2d7390300eeefb8f
Peter Singh <peter.singh@freeagent.com>
2021-08-03 15:10:01 +0100

    Add first basic test for index.js (the action)

--- index.test.js
+++ index.test.js
@@ -1,3 +1,11 @@
 test('adds 1 + 2 to equal 3', () => {
     expect(1 + 2).toBe(3);
 });
+
+test('index runs the action', () => {
+    const consoleSpy = jest.spyOn(console, 'log');
+
+    const index = require('./index');
+
+    expect(consoleSpy).toHaveBeenCalledWith('Deploying application: () at ');
+})
