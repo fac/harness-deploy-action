e40814f6f14adea824d34bbf08e34bc8ae08df23
Peter Singh <peter.singh@freeagent.com>
2021-08-05 15:36:22 +0100

    Add basic index.js tests

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const core = require('@actions/core');
 const HarnessDeployment = require("./harness-deployment");
 
 try {
-  const harnessDeployment = new HarnessDeployment
-  harnessDeployment.start(
+  const harnessDeployment = new HarnessDeployment(
     core.getInput('webhookUrl'),
     core.getInput('application'),
     core.getInput('version'),
     core.getInput('services')
   );
+
+  harnessDeployment.start();
 } catch (error) {
   core.setFailed(error.message);
 }
--- index.test.js
+++ index.test.js
@@ -1,4 +1,20 @@
 const core = require('@actions/core');
+let HarnessDeployment = require("./harness-deployment");
+jest.mock("./harness-deployment");
+
 test('calls harnessDeployment with the passed args', () => {
+    const startSpy = jest.spyOn(HarnessDeployment.prototype, 'start');
+
+    process.env['INPUT_APPLICATION'] = "Test Application";
+    process.env['INPUT_SERVICES'] = "web, job";
+    process.env['INPUT_WEBHOOKURL'] = "https://www.examplewebhookurl.com";
+    process.env['INPUT_VERSION'] = "v1";
+
     const index = require('./index.js');
+
+    expect(HarnessDeployment).toHaveBeenCalledWith(
+        "https://www.examplewebhookurl.com", "Test Application", "v1", "web, job"
+    );
+
+    expect(startSpy).toHaveBeenCalledTimes(1);
 });
