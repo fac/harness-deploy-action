a777beac31d5c15da7a2f6991c50a3149a0a1d30
Daniel Holz <dgholz@gmail.com>
2021-08-11 16:05:42 +0100

    Set up Actions input for integration test

--- entrypoint.test.js
+++ entrypoint.test.js
@@ -1,7 +1,12 @@
 test('runs the action', () => {
     const consoleSpy = jest.spyOn(console, 'log');
 
+    process.env['INPUT_APPLICATION'] = 'pincushion';
+    process.env['INPUT_SERVICES'] = 'pin, cush ,ion';
+    process.env['INPUT_WEBHOOKURL'] = 'https://example.com/harness/webhook';
+    process.env['INPUT_VERSION'] = 'v0';
+
     const action = require('./entrypoint');
 
-    expect(consoleSpy).toHaveBeenCalledWith('Deploying application: () at ');
+    expect(consoleSpy).toHaveBeenCalledWith('Deploying application:pincushion (pin, cush ,ion) at v0');
 })
